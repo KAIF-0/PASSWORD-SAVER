6e5bf332e54439b3cb468edac092f74c1cef5326
KAIF 0 <kaif8700979251@gmail.com>
2024-07-09 21:24:07 +0530

    1 commit

--- src/app/api/(pass)/getpass/route.js
+++ src/app/api/(pass)/getpass/route.js
@@ -3,7 +3,6 @@ import prisma from "@/app/lib/prisma";
 import { cookies } from "next/headers";
 import jwt from "jsonwebtoken";
 
-
 const verifyJWT = (token, secret) => {
     return new Promise((resolve, reject) => {
         jwt.verify(token, secret, (err, decoded) => {
@@ -17,35 +16,25 @@ const verifyJWT = (token, secret) => {
 };
 
 export async function GET(request, { params }) {
-    const cookieStore = cookies();
+    const cookieStore = cookies(request.headers);
     const cookie = cookieStore.get('authToken');
 
     try {
         if (cookie) {
-            const { value } = cookie;
-            const token = value;
-
+            const token = cookie.value;
             const user = await verifyJWT(token, process.env.JWT_SECRET);
 
-            const passwords = await prisma.password.findMany({ where: { userid: user.userid } })
+            const passwords = await prisma.password.findMany({ where: { userid: user.userid } });
 
-            if (passwords) {
-                const data = passwords
-                return NextResponse.json(data, { status: 200 });
+            if (passwords.length > 0) {
+                return NextResponse.json(passwords, { status: 200 });
             } else {
                 return NextResponse.json({ message: "No documents found" }, { status: 404 });
             }
         } else {
-            return NextResponse.json(
-                { message: "You are not logged in" },
-                { status: 401 }
-            );
+            return NextResponse.json({ message: "You are not logged in" }, { status: 401 });
         }
     } catch (error) {
-        return NextResponse.json(
-            { message: error.message },
-            { status: 400 }
-        );
+        return NextResponse.json({ message: error.message }, { status: 400 });
     }
-
 }
